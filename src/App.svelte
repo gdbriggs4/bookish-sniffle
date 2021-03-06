3382e3ad0a34bc3ff7899ba7874352d5f4eedb97
Guy Briggs <gdbriggs4@gmail.com>
2021-03-05 19:00:42 -0600

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script>
+	export let name;
+</script>
+
+<main>
+	<h1>Hello {name}!</h1>
+	<h2 class="text-red-500">Testing TailwindCSS</h2>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+</main>
+
+<style>
+	
+</style>
